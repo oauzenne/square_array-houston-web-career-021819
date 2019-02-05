cfae2a2020022b64545975cabd34b0fe3055cf43
Olivia Auzenne <oauzenne@gmail.com>
2019-02-05 20:50:08 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,7 +1,11 @@
 require 'pry'
 def square_array(numbers)
-binding.pry  
   new_array = []
-  numbers.each{|item|new_array<< item**2}
+#  numbers.each{|item|new_array<< item**2}
+
+  numbers.each do |number|
+    binding.pry
+    new_array.push(number * number)
+  end
 new_array
 end
